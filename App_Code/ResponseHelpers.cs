6b24f6db8799334f1c988fb0a2fe5a7178db94c0
Weeks, Andrew D <andrew.weeks@tdameritrade.com>
2014-12-03 16:10:08 -0600

    Making It JsonP Compliant

--- App_Code/ResponseHelpers.cs
+++ App_Code/ResponseHelpers.cs
@@ -12,7 +12,12 @@ namespace WeeksDev
     {
         public static void WriteJsonResponse(this object data, string message = "", string header = "", bool success = true)
         {
-            HttpContext.Current.Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(new { header = header, message = message, data = data, success = success }));
+            string callback = HttpContext.Current.Request.Params.Get("callback");
+            if (callback == null || callback == "") {
+                HttpContext.Current.Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(new { header = header, message = message, data = data, success = success }));
+            } else {
+                HttpContext.Current.Response.Write(callback + "(" + Newtonsoft.Json.JsonConvert.SerializeObject(new { header = header, message = message, data = data, success = success }) + ")");
+            }
         }
     }
 }
